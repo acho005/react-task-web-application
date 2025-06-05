0dbd1a0314d70aa7f0bdc8dc4867707f858e8d81
Alan <alan_cho_dillon@hotmail.com>
2025-06-05 18:43:50 -0400

    first commit

---
+++ src/data/tasks.jsx
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "My first task",
+    description: "First Task",
+  },
+  {
+    id: 1,
+    title: "My second task",
+    description: "Second Task",
+  },
+  {
+    id: 2,
+    title: "My third task",
+    description: "Third Task",
+  },
+  {
+    id: 3,
+    title: "My fourth task",
+    description: "Fourth Task",
+  },
+];
